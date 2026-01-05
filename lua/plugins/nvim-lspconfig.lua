fdf9b509da3d7c99d6524c716d561a350c45c31e
OrkanGoekcen <orkan.gok@gmail.com>
2026-01-05 21:02:42 +0000

    configured lsp hotkeys

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -18,23 +18,9 @@ return {
           vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
         end
 
-        map('grn', vim.lsp.buf.rename, '[R]e[n]ame')
-
-        map('gra', vim.lsp.buf.code_action, '[G]oto Code [A]ction', { 'n', 'x' })
-
-        map('grr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-
-        map('gri', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-
-        map('grd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-
-        map('grD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-
-        map('gO', require('telescope.builtin').lsp_document_symbols, 'Open Document Symbols')
-
-        map('gW', require('telescope.builtin').lsp_dynamic_workspace_symbols, 'Open Workspace Symbols')
-
-        map('grt', require('telescope.builtin').lsp_type_definitions, '[G]oto [T]ype Definition')
+        map('<leader>gr', vim.lsp.buf.code_action, '[G]oto Code [A]ction', { 'n', 'x' })
+        map('<leader>gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+        map('<leader>gi', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
 
         ---@param client vim.lsp.Client
         ---@param method vim.lsp.protocol.Method
