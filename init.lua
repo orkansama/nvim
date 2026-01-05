e79e92ebddca1fd1653f8e01b213050611e0b8b7
OrkanGoekcen <orkan.gok@gmail.com>
2026-01-05 21:20:10 +0000

    added logic to create window

--- init.lua
+++ init.lua
@@ -42,17 +42,43 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
+vim.keymap.set('n', '<leader>bo', function() vim.cmd('silent! %bd|e#|bd#') end, { desc = 'Close other buffers' })
+
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+local function smart_move(dir)
+  local curwin = vim.api.nvim_get_current_win()
+  local target
+
+  -- prüfen ob Fenster existiert (nur Fokus!)
+  vim.cmd('wincmd ' .. dir:lower())
+  target = vim.api.nvim_get_current_win()
+
+  if target == curwin then
+    -- kein Fenster → Split erzeugen
+    if dir == 'H' or dir == 'L' then
+      vim.cmd('vsplit')
+    else
+      vim.cmd('split')
+    end
+  else
+    -- zurück zum Original
+    vim.api.nvim_set_current_win(curwin)
+  end
+
+  -- jetzt sicher verschieben
+  vim.cmd('wincmd ' .. dir)
+end
 
 vim.keymap.set('n', '<leader>wh', '<C-w>h', { desc = 'Focus left window' })
 vim.keymap.set('n', '<leader>wl', '<C-w>l', { desc = 'Focus right window' })
 vim.keymap.set('n', '<leader>wj', '<C-w>j', { desc = 'Focus lower window' })
 vim.keymap.set('n', '<leader>wk', '<C-w>k', { desc = 'Focus upper window' })
 
-vim.keymap.set('n', '<leader>wH', '<C-w>H', { desc = 'Move window to left' })
-vim.keymap.set('n', '<leader>wL', '<C-w>L', { desc = 'Move window to right' })
-vim.keymap.set('n', '<leader>wJ', '<C-w>J', { desc = 'Move window to bottom' })
-vim.keymap.set('n', '<leader>wK', '<C-w>K', { desc = 'Move window to top' })
+vim.keymap.set('n', '<leader>wH', function() smart_move('H') end, { desc = 'Move window left (or create)' })
+vim.keymap.set('n', '<leader>wL', function() smart_move('L') end, { desc = 'Move window right (or create)' })
+vim.keymap.set('n', '<leader>wJ', function() smart_move('J') end, { desc = 'Move window down (or create)' })
+vim.keymap.set('n', '<leader>wK', function() smart_move('K') end, { desc = 'Move window up (or create)' })
 
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
