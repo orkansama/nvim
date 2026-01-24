ee0e8ac08780f19fd5edf95ef15a39ce1190c7e0
OrkanGoekcen <orkan.gok@gmail.com>
2026-01-24 23:12:45 +0100

    theme.lua now works correctly

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,14 +1,11 @@
 return {
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-	opts = {
-		flavour = "mocha",
-	},
-    config = function()
-      require("catppuccin").setup(opts)
-      vim.cmd.colorscheme "catppuccin"
-    end,
-  },
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
+  config = function()
+    require("catppuccin").setup({
+      flavour = "mocha"
+    })
+    vim.cmd.colorscheme "catppuccin"
+  end,
 }
