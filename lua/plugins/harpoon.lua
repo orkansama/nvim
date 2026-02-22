64092c94ea81b03387f3d8084e78bf14cfcec190
OrkanGoekcen <orkan.gok@gmail.com>
2026-02-22 03:11:25 +0100

    Changed which-key for the better of harpoon

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -3,6 +3,15 @@ return {
   dependencies = {
     'nvim-lua/plenary.nvim',
   },
-
-  vim.keymap.set('n', '<leader>hh', '<cmd>:lua require("harpoon.ui").toggle_quick_menu()<CR>', { noremap = true, silent = true }),
+  vim.keymap.set('n', '<leader>ha', '<cmd>:lua require("harpoon.mark").add_file()<CR>', { noremap = true, silent = true, desc = 'harpoon [a]dd to list' }),
+  vim.keymap.set('n', '<leader>hh', '<cmd>:lua require("harpoon.ui").toggle_quick_menu()<CR>', { noremap = true, silent = true, desc = 'harpoon show list' }),
+  vim.keymap.set('n', '<leader>h1', '<cmd>:lua require("harpoon.ui").nav_file(1)<CR>', { noremap = true, silent = true, desc = 'harpoon[1] Jump' }),
+  vim.keymap.set('n', '<leader>h2', '<cmd>:lua require("harpoon.ui").nav_file(2)<CR>', { noremap = true, silent = true, desc = 'harpoon[2] Jump' }),
+  vim.keymap.set('n', '<leader>h3', '<cmd>:lua require("harpoon.ui").nav_file(3)<CR>', { noremap = true, silent = true, desc = 'harpoon[3] Jump' }),
+  vim.keymap.set('n', '<leader>h4', '<cmd>:lua require("harpoon.ui").nav_file(4)<CR>', { noremap = true, silent = true, desc = 'harpoon[4] Jump' }),
+  vim.keymap.set('n', '<leader>h5', '<cmd>:lua require("harpoon.ui").nav_file(5)<CR>', { noremap = true, silent = true, desc = 'harpoon[5] Jump' }),
+  vim.keymap.set('n', '<leader>h6', '<cmd>:lua require("harpoon.ui").nav_file(6)<CR>', { noremap = true, silent = true, desc = 'harpoon[6] Jump' }),
+  vim.keymap.set('n', '<leader>h7', '<cmd>:lua require("harpoon.ui").nav_file(7)<CR>', { noremap = true, silent = true, desc = 'harpoon[7] Jump' }),
+  vim.keymap.set('n', '<leader>h8', '<cmd>:lua require("harpoon.ui").nav_file(8)<CR>', { noremap = true, silent = true, desc = 'harpoon[8] Jump' }),
+  vim.keymap.set('n', '<leader>h9', '<cmd>:lua require("harpoon.ui").nav_file(9)<CR>', { noremap = true, silent = true, desc = 'harpoon[9] Jump' }),
 }
