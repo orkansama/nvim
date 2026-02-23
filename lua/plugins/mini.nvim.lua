9ab9e2c5bc8eb5b997f310f4bdcfd00e3232dbcf
OrkanGoekcen <orkan.gok@gmail.com>
2026-02-23 13:47:37 +0100

    removed      require('mini.surround').setup() from mini.nvim.lua

--- lua/plugins/mini.nvim.lua
+++ lua/plugins/mini.nvim.lua
@@ -1,8 +1,6 @@
   return {
     'echasnovski/mini.nvim',
     config = function()
-      require('mini.surround').setup()
-
       local statusline = require 'mini.statusline'
       statusline.setup { use_icons = vim.g.have_nerd_font }
 
