f50a1270858a11a1827a73dc1a44967a3dcd5ed8
OrkanGoekcen <orkan.gok@gmail.com>
2026-03-16 11:38:23 +0100

    added custom comment keymaps to mini.nvim.lua

--- lua/plugins/mini.nvim.lua
+++ lua/plugins/mini.nvim.lua
@@ -1,12 +1,23 @@
-  return {
-    'echasnovski/mini.nvim',
-    config = function()
-      local statusline = require 'mini.statusline'
-      statusline.setup { use_icons = vim.g.have_nerd_font }
+return {
+  'echasnovski/mini.nvim',
+  config = function()
+    local statusline = require 'mini.statusline'
+    statusline.setup { use_icons = vim.g.have_nerd_font }
+    ---@diagnostic disable-next-line: duplicate-set-field
+    statusline.section_location = function()
+      return '%2l:%-2v'
+    end
 
-      ---@diagnostic disable-next-line: duplicate-set-field
-      statusline.section_location = function()
-        return '%2l:%-2v'
-      end
-    end,
-  }
+    require('mini.comment').setup {
+      hooks = {
+        post = function()
+          vim.cmd 'normal! gv'
+        end,
+      },
+      mappings = {
+        comment_line = '<C-#>',
+        comment_visual = '<C-#>',
+      },
+    }
+  end,
+}
