f163299479ca024f29a070618d8cbda966247af8
OrkanGoekcen <orkan.gok@gmail.com>
2026-03-16 13:37:11 +0100

    Added desc to multiline.lua

--- lua/plugins/multiline.lua
+++ lua/plugins/multiline.lua
@@ -1,6 +1,6 @@
 return {
   'mg979/vim-visual-multi',
   branch = 'master',
-  vim.keymap.set('n', '<leader>ln', '<Plug>(VM-Find-Under)', {}),
-  vim.keymap.set('n', '<leader>la', '<Plug>(VM-Select-All)', {}),
+  vim.keymap.set('n', '<leader>ln', '<Plug>(VM-Find-Under)', { desc = 'Select [N]ext [L]ine Occurrence' }),
+  vim.keymap.set('n', '<leader>la', '<Plug>(VM-Select-All)', { desc = 'Select [A]ll [L]ine Occurrence' }),
 }
