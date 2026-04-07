8f052bdedf68e6e4585a99ca2c9366858f5a611d
OrkanGoekcen <orkan.gok@gmail.com>
2026-04-07 17:38:19 +0200

    added lazyvim configuration to init.lua

--- init.lua
+++ init.lua
@@ -45,3 +45,26 @@ vim.keymap.set('n', '<leader>wH', function() smart_move('H') end, { desc = 'Move
 vim.keymap.set('n', '<leader>wL', function() smart_move('L') end, { desc = 'Move window right (or create)' })
 vim.keymap.set('n', '<leader>wJ', function() smart_move('J') end, { desc = 'Move window down (or create)' })
 vim.keymap.set('n', '<leader>wK', function() smart_move('K') end, { desc = 'Move window up (or create)' })
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({ spec = {
+	{ import = "plugins" }
+  },
+  -- automatically check for plugin updates
+  checker = { enabled = true },
+})
