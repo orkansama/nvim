eeee784f0a9f81cb4529494e2de48f91fdfe531c
OrkanGoekcen <orkan.gok@gmail.com>
2026-04-07 17:24:34 +0200

    added smart window move functions to init.lua

--- init.lua
+++ init.lua
@@ -1,7 +1,8 @@
 vim.g.mapleader = ' '
 
 vim.opt.relativenumber = true
-vim.opt.number = true
+
+vim.opt.showmode = false
 
 vim.opt.clipboard = 'unnamedplus'
 
@@ -14,3 +15,33 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 
 vim.opt.hlsearch = true
 vim.keymap.set('n', '<leader>n', '<cmd>:nohlsearch<CR>')
+
+local function smart_move(dir)
+  local curwin = vim.api.nvim_get_current_win()
+  local target
+
+  vim.cmd('wincmd ' .. dir:lower())
+  target = vim.api.nvim_get_current_win()
+
+  if target == curwin then
+    if dir == 'H' or dir == 'L' then
+      vim.cmd('vsplit')
+    else
+      vim.cmd('split')
+    end
+  else
+    vim.api.nvim_set_current_win(curwin)
+  end
+
+  vim.cmd('wincmd ' .. dir)
+end
+
+vim.keymap.set('n', '<leader>wh', '<C-w>h', { desc = 'Focus left window' })
+vim.keymap.set('n', '<leader>wl', '<C-w>l', { desc = 'Focus right window' })
+vim.keymap.set('n', '<leader>wj', '<C-w>j', { desc = 'Focus lower window' })
+vim.keymap.set('n', '<leader>wk', '<C-w>k', { desc = 'Focus upper window' })
+
+vim.keymap.set('n', '<leader>wH', function() smart_move('H') end, { desc = 'Move window left (or create)' })
+vim.keymap.set('n', '<leader>wL', function() smart_move('L') end, { desc = 'Move window right (or create)' })
+vim.keymap.set('n', '<leader>wJ', function() smart_move('J') end, { desc = 'Move window down (or create)' })
+vim.keymap.set('n', '<leader>wK', function() smart_move('K') end, { desc = 'Move window up (or create)' })
