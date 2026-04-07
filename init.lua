62129a963c3073006c9c2acc2c6583f13e5ee9c7
OrkanGoekcen <orkan.gok@gmail.com>
2026-04-07 06:20:02 +0000

    added a few basic settings to init.lua

--- init.lua
+++ init.lua
@@ -1,1 +1,10 @@
-vim.g.mapleader = " "
+vim.opt.relativenumber = true
+vim.opt.number = true
+
+vim.opt.clipboard = unnamedplus
+
+vim.opt.hlsearch = true
+
+vim.g.mapleader = ' '
+vim.keymap.set('n', '<leader>n', '<cmd>:nohlsearch<CR>')
+
