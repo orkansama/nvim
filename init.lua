84fe07dfce301ad50c6b9bb8cc14d4883c4a99ca
OrkanGoekcen <orkan.gok@gmail.com>
2026-04-07 19:01:24 +0000

    Added Ex keybind to init.lua

--- init.lua
+++ init.lua
@@ -22,6 +22,8 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 vim.opt.hlsearch = true
 vim.keymap.set('n', '<leader>n', '<cmd>:nohlsearch<CR>')
 
+vim.keymap.set('n', '<leader>e', '<cmd>:Ex<CR>')
+
 local function smart_move(dir)
   local curwin = vim.api.nvim_get_current_win()
   local target
