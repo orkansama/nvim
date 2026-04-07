d5dbdf58f8431f89fbf33a0a87121c8c275b290d
OrkanGoekcen <orkan.gok@gmail.com>
2026-04-07 17:10:43 +0200

    Added highlight on yank to config

--- init.lua
+++ init.lua
@@ -5,5 +5,12 @@ vim.opt.number = true
 
 vim.opt.clipboard = 'unnamedplus'
 
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank({ timeout = 100 })
+  end,
+})
+
+
 vim.opt.hlsearch = true
 vim.keymap.set('n', '<leader>n', '<cmd>:nohlsearch<CR>')
