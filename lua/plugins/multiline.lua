5015511922a730e6f19eb89086a23f41990333eb
OrkanGoekcen <orkan.gok@gmail.com>
2026-04-19 07:13:23 +0000

    lua/plugins/multiline.lua added

---
+++ lua/plugins/multiline.lua
@@ -0,0 +1,7 @@
+return {
+  'mg979/vim-visual-multi',
+  branch = 'master',
+  vim.keymap.set('n', '<leader>ln', '<Plug>(VM-Find-Under)', { desc = 'Select [N]ext [L]ine Occurrence' }),
+  vim.keymap.set('n', '<leader>la', '<Plug>(VM-Select-All)', { desc = 'Select [A]ll [L]ine Occurrence' }),
+  vim.g.VM_theme == "iceblue"
+}
