fa02abb5d92fa026b679d1caece5ed4b0134d4e0
OrkanGoekcen <orkan.gok@gmail.com>
2026-04-25 19:52:47 +0000

    lua/plugins/lsp-config.lua has more config now

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,11 +1,17 @@
 return {
     "neovim/nvim-lspconfig",
     dependencies = {
-        "mason-org/mason.nvim"
+        "mason-org/mason.nvim",
+        "mason-org/mason-lspconfig.nvim"
     },
+    servers = {"lua_ls"};
     config = function()
         require('mason').setup()
-        vim.lsp.config("*", {})
-        vim.lsp.enable("lua_ls")
+        require("mason-lspconfig").setup
+        {
+            ensure_installed = { "lua_ls" },
+            automatic_enable = true,
+        }
+        vim.lsp.config("*", { })
     end
 }
