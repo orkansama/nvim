a821d22cbf028cd2bf709d36d330527d7978ac7d
OrkanGoekcen <orkan.gok@gmail.com>
2026-04-25 21:25:26 +0000

    added keymaps for lua/plugins/lsp-config.lua

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -12,5 +12,14 @@ return {
             ensure_installed = { "lua_ls" },
             automatic_enable = true,
         }
+
+        -- Keymaps
+        local telescope = require('telescope.builtin')
+        vim.keymap.set('n', '<leader>gr', telescope.lsp_references, { desc = 'LSP References' })
+        vim.keymap.set('n', '<leader>gd', telescope.lsp_definitions, { desc = 'LSP Definitions' })
+
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, { desc = 'LSP Hover' })
+        vim.keymap.set('n', '<leader>gi', vim.lsp.buf.implementation, { desc = 'LSP implementation' })
+        vim.keymap.set('n', '<leader>gd', vim.lsp.buf.rename, { desc = 'LSP Rename' })
     end
 }
