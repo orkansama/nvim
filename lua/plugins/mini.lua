b70fb668e71c9767c801ba0e439700847dd24f67
OrkanGoekcen <orkan.gok@gmail.com>
2026-04-26 11:09:44 +0000

    lua/plugins/mini.lua now has extra miniComment local variable

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,7 +1,15 @@
 return {
     "echasnovski/mini.nvim",
     config = function()
-        require('mini.comment').setup {
+        local statusline = require('mini.statusline')
+        local miniComment = require('mini.comment')
+
+        statusline.setup { use_icons = vim.g.have_nerd_font }
+        statusline.section_location = function()
+          return '%2l:%-2v'
+        end
+
+        miniComment.setup {
           hooks = {
             post = function()
               vim.cmd 'normal! gv'
