73b50fd6d4e55973710a585a42981f7fc2975c1a
OrkanGoekcen <orkan.gok@gmail.com>
2026-04-28 15:40:26 +0200

    lua/plugins/nvim-cmp.lua configured

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -8,4 +8,23 @@ return {
         'saadparwaiz1/cmp_luasnip'
     },
     Lazy = false,
+
+    config = function()
+        local cmp = require("cmp")
+        cmp.setup({
+          mapping = cmp.mapping.preset.insert({
+            ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+            ['<C-f>'] = cmp.mapping.scroll_docs(4),
+            ['<C-Space>'] = cmp.mapping.complete(),
+            ['<ENTER>'] = cmp.mapping.confirm({ select = true }),
+          }),
+
+          sources = cmp.config.sources({
+            { name = 'nvim_lsp' },
+            { name = 'nvim_lua' },
+            { name = 'luasnip' },
+            { name = 'buffer' },
+          })
+      })
+    end
 }
