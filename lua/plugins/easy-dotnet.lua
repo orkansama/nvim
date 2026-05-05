eb27dfd7713bf91c1c5df3ea8c4fc72ca28a1fc3
OrkanGoekcen <orkan.gok@gmail.com>
2026-05-05 11:10:23 +0200

    changed a few settings in lua/plugins/easy-dotnet.lua

--- lua/plugins/easy-dotnet.lua
+++ lua/plugins/easy-dotnet.lua
@@ -1,56 +1,22 @@
 return {
   "GustavEikaas/easy-dotnet.nvim",
-  -- 'nvim-telescope/telescope.nvim' or 'ibhagwan/fzf-lua' or 'folke/snacks.nvim'
-  -- are highly recommended for a better experience
-  dependencies = { "nvim-lua/plenary.nvim", 'mfussenegger/nvim-dap', 'folke/snacks.nvim', },
+  dependencies = {
+      'nvim-lua/plenary.nvim',
+      'mfussenegger/nvim-dap',
+      'folke/snacks.nvim',
+  },
   config = function()
     local dotnet = require("easy-dotnet")
-    -- Options are not required
     dotnet.setup({
-     managed_terminal = {
-       auto_hide = true, -- auto hides terminal if exit code is 0
-       auto_hide_delay = 1000, -- delay before auto hiding, 0 = instant
-       mappings = {
-         next_tab       = { lhs = "<Tab>",   desc = "Next terminal tab" },
-         prev_tab       = { lhs = "<S-Tab>", desc = "Previous terminal tab" },
-         new_terminal   = { lhs = "+",       desc = "New user terminal" },
-         close_terminal = { lhs = "X",       desc = "Close current terminal tab" },
-         hide_panel     = { lhs = "q",       desc = "Hide terminal panel" },
-       },
-     },
-      -- Optional configuration for external terminals (matches nvim-dap structure)
       external_terminal = nil,
       lsp = {
-        enabled = true, -- Enable builtin roslyn lsp
-        set_fold_expr = false,
-        preload_roslyn = true, -- Start loading roslyn before any buffer is opened
-        roslynator_enabled = true, -- Automatically enable roslynator analyzer
-        easy_dotnet_analyzer_enabled = true, -- Enable roslyn analyzer from easy-dotnet-server
-        auto_refresh_codelens = true,
-        analyzer_assemblies = {}, -- Any additional roslyn analyzers you might use like SonarAnalyzer.CSharp
-        config = {},
+        enabled = false, -- Enable builtin roslyn lsp
       },
-      debugger = {
-        -- Path to custom coreclr DAP adapter
-        -- easy-dotnet-server falls back to its own netcoredbg binary if bin_path is nil
-        bin_path = nil,
-        console = "integratedTerminal", -- Controls where the target app runs: "integratedTerminal" (Neovim buffer) or "externalTerminal" (OS window)
-        apply_value_converters = true,
-        auto_register_dap = true,
-        mappings = {
-          open_variable_viewer = { lhs = "T", desc = "open variable viewer" },
-        },
-      },
+
+      vim.keymap.set('n', '<leader>tt', '<cmd>:Dotnet testrunner<CR>'),
       ---@type TestRunnerOptions
       test_runner = {
         auto_start_testrunner = true,
-        hide_legend = false,
-        ---@type "split" | "vsplit" | "float" | "buf"
-        viewmode = "float",
-        ---@type number|nil
-        vsplit_width = nil,
-        ---@type string|nil "topleft" | "topright" 
-        vsplit_pos = nil,
         icons = {
           passed = "",
           skipped = "",
@@ -66,16 +32,14 @@ return {
           build_failed = "󰒡",
         },
         mappings = {
-          run_test_from_buffer = { lhs = "<leader>r", desc = "run test from buffer" },
-          run_all_tests_from_buffer = { lhs = "<leader>t", desc = "Run all tests in file" },
-          get_build_errors = { lhs = "<leader>e", desc = "get build errors" },
-          peek_stack_trace_from_buffer = { lhs = "<leader>p", desc = "peek stack trace from buffer" },
-          debug_test_from_buffer = { lhs = "<leader>d", desc = "run test from buffer" },
-          debug_test = { lhs = "<leader>d", desc = "debug test" },
-          go_to_file = { lhs = "g", desc = "go to file" },
-          run_all = { lhs = "<leader>R", desc = "run all tests" },
-          run = { lhs = "<leader>r", desc = "run test" },
-          peek_stacktrace = { lhs = "<leader>p", desc = "peek stacktrace of failed test" },
+          -- buffer
+          run_all_tests_from_buffer = { lhs = "<leader>tf", desc = "Run all tests in file" },
+          debug_test = { lhs = "<leader>td", desc = "debug test" },
+          go_to_file = { lhs = "<leader>gi", desc = "go to file" },
+          run_all = { lhs = "<leader>ta", desc = "run all tests" },
+          run = { lhs = "<leader>tr", desc = "run test" },
+          peek_stacktrace = { lhs = "<leader>q", desc = "peek stacktrace of failed test" },
+
           expand = { lhs = "o", desc = "expand" },
           expand_node = { lhs = "E", desc = "expand node" },
           collapse_all = { lhs = "W", desc = "collapse all" },
@@ -86,18 +50,17 @@ return {
       },
       new = {
         project = {
-          prefix = "sln" -- "sln" | "none"
+          prefix = "sln"
         }
       },
       csproj_mappings = true,
       fsproj_mappings = true,
       auto_bootstrap_namespace = {
-          --block_scoped, file_scoped
           type = "block_scoped",
           enabled = true,
           use_clipboard_json = {
-            behavior = "prompt", --'auto' | 'prompt' | 'never',
-            register = "+", -- which register to check
+            behavior = "prompt",
+            register = "+",
           },
       },
       server = {
@@ -109,39 +72,7 @@ return {
       -- if no picker is specified, the plugin will determine
       -- the available one automatically with this priority:
       --  snacks -> fzf -> telescope ->  basic
-      picker = "snacks",
-      background_scanning = true,
-      notifications = {
-        --Set this to false if you have configured lualine to avoid double logging
-        handler = function(start_event)
-          local spinner = require("easy-dotnet.ui-modules.spinner").new()
-          spinner:start_spinner(start_event.job.name)
-          ---@param finished_event JobEvent
-          return function(finished_event)
-            spinner:stop_spinner(finished_event.result.msg, finished_event.result.level)
-          end
-        end,
-      },
-      diagnostics = {
-        default_severity = "error",
-        setqflist = false,
-      },
-      outdated = {
-        mappings = {
-          upgrade = { lhs = "<leader>pu", desc = "upgrade package under cursor" },
-          upgrade_all = { lhs = "<leader>pa", desc = "upgrade all outdated packages" },
-        },
-      },
+      picker = "telescope",
     })
-
-    -- Example command
-    vim.api.nvim_create_user_command('Secrets', function()
-      dotnet.secrets()
-    end, {})
-
-    -- Example keybinding
-    vim.keymap.set("n", "<C-p>", function()
-      dotnet.run_project()
-    end)
   end
 }
