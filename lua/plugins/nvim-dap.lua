bc44062e1a8db6e5bd52a6bc47e18e813969ebc1
OrkanGoekcen <orkan.gok@gmail.com>
2026-05-05 14:57:00 +0200

    added lua/plugins/nvim-dap.lua

---
+++ lua/plugins/nvim-dap.lua
@@ -0,0 +1,46 @@
+return {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+        "mason-org/mason.nvim",
+        "jay-babu/mason-nvim-dap.nvim",
+    },
+    config = function()
+        local dap = require('dap')
+
+        dap.adapters.coreclr = {
+          type = 'executable',
+          command = 'netcoredbg',
+          args = { '--interpreter=vscode' },
+        }
+
+        dap.configurations.cs = {
+          {
+            name = '.NET Aspire (AppHost)',
+            type = 'coreclr',
+            request = 'launch',
+            program = 'dotnet',
+            args = {
+              'run',
+              '--project',
+              '${workspaceFolder}/CL.API.AppHost',
+              '--launch-profile',
+              'https',
+            },
+            cwd = '${workspaceFolder}/CL.API.AppHost',
+            stopAtEntry = false,
+          },
+        }
+
+        require("mason-nvim-dap").setup({
+            ensure_installed = { "netcoredbg" },
+        })
+
+    vim.keymap.set('n', '<F5>', function() require('dap').continue() end)
+    vim.keymap.set('n', '<F10>', function() require('dap').step_over() end)
+    vim.keymap.set('n', '<F11>', function() require('dap').step_into() end)
+    vim.keymap.set('n', '<F12>', function() require('dap').step_out() end)
+    vim.keymap.set('n', '<Leader>b', function() require('dap').toggle_breakpoint() end)
+    -- vim.keymap.set('n', '<leader>da', '<cmd>:DapNew Launch<CR>')
+    -- vim.keymap.set('n', '<leader>da', '<cmd>:DapDisconnect<CR>')
+    end
+}
