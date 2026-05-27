7a309a92eef989526b27a56dd97875c30f573502
OrkanGoekcen <orkan.gok@gmail.com>
2026-05-27 18:38:27 +0000

    feat(Claude): Added Claude-Code to neovim config

---
+++ lua/plugins/claude-code.lua
@@ -0,0 +1,14 @@
+return {
+    "coder/claudecode.nvim",
+    dependencies = { "folke/snacks.nvim" },
+    config = true,
+    keys = {
+        { "<leader>ii", "<cmd>ClaudeCodeFocus<cr>", desc = "Focus Claude" },
+        { "<leader>as", "<cmd>ClaudeCodeTreeAdd<cr>", desc = "Add file", ft = { "NvimTree", "neo-tree", "oil", "minifiles", "netrw" },},
+
+        -- Diff management
+        { "<leader>ia", "<cmd>ClaudeCodeDiffAccept<cr>", desc = "Accept diff" },
+        { "<leader>id", "<cmd>ClaudeCodeDiffDeny<cr>", desc = "Deny diff" },
+    },
+
+}
