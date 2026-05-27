40212faebe8aff993ca325571f1063b2d9b650be
OrkanGoekcen <orkan.gok@gmail.com>
2026-05-27 18:01:47 +0000

    feat(git): Git-History keybinds implemented

--- lua/plugins/git-keybinds.lua
+++ lua/plugins/git-keybinds.lua
@@ -1,4 +1,22 @@
 return {
+    "LionyxML/gitlineage.nvim",
+    config = function()
+        require("gitlineage").setup({
+            keymap = "nil",
+            keys = {
+                next_commit = "+c",
+                prev_commit = "üc",
+            },
+        })
+    end,
+
+    vim.keymap.set({'n', 'v'}, '<leader>zl', '<cmd>:GitLineage<CR>', { desc = "View Line History" }),
+
+    vim.keymap.set('n', '<leader>zh', function()
+        vim.cmd('normal! ggVG')
+        vim.cmd("'<,'>GitLineage")
+    end, { desc = 'View File History' }),
+
     vim.keymap.set({'n', 'v'}, '<leader>zb', function()
         local branchName = vim.fn.input("Enter branch: ")
         local fileName = vim.fn.bufname("%")
