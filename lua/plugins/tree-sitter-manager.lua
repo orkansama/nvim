5a9f05f7a8fe14f7ef7849fe473d90e868b2b45b
OrkanGoekcen <orkan.gok@gmail.com>
2026-05-28 13:03:33 +0200

    feat(TreeSitter): Added Keymap for InspectTree

--- lua/plugins/tree-sitter-manager.lua
+++ lua/plugins/tree-sitter-manager.lua
@@ -1,10 +1,13 @@
 return {
-    "romus204/tree-sitter-manager.nvim",
-    dependencies = {},
-    config = function()
-        require("tree-sitter-manager").setup({
-            ensure_installed = {"c_sharp", "lua"},
-            nohighlight = {"c_sharp", "lua"},
-        })
-    end
+  'romus204/tree-sitter-manager.nvim',
+  dependencies = {},
+  config = function()
+    require('tree-sitter-manager').setup {
+      ensure_installed = { 'c_sharp', 'lua' },
+      nohighlight = { 'c_sharp', 'lua' },
+    }
+  end,
+  vim.keymap.set('n', '<leader>xo', function()
+    vim.treesitter.inspect_tree()
+  end),
 }
