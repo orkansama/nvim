67328cc768cbe390e353d08767da3821621565ad
OrkanGoekcen <orkan.gok@gmail.com>
2026-06-03 08:24:13 +0200

    feat(Git): Changed filenames, keymap positions and keymap implementations

--- lua/plugins/simple-file-compare.lua
+++ lua/plugins/simple-file-compare.lua
@@ -6,9 +6,12 @@ return {
     'nvim-mini/mini.pick',
   },
   config = function()
-    local simpleFileCompare = require('simple-file-compare')
-    simpleFileCompare.setup({
+    local simpleFileCompare = require 'simple-file-compare'
+    simpleFileCompare.setup {
       mode = 'snacks', -- "snacks" | "vimUiSelect" | "miniPick"
-    })
+    }
   end,
+
+  vim.keymap.set({ 'n', 'v' }, '<leader>zb', '<cmd>:FileCompareOpen<CR>', { desc = 'Compare Current File With Other Branch' }),
+  vim.keymap.set({ 'n', 'v' }, '<leader>zc', '<cmd>:FileCompareClose<CR>', { desc = 'Close Branch Comparison' }),
 }
